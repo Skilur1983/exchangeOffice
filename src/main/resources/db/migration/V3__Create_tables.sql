94b9af9af83cb01eadb9bc29e4af8192018b1fad
oleksich <d.oleksich@gmail.com>
2025-12-25 21:41:40 +0100

    Updated user table migration

--- src/main/resources/db/migration/V3__Create_tables.sql
+++ src/main/resources/db/migration/V3__Create_tables.sql
@@ -1,10 +1,20 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto SCHEMA office;
 
 CREATE TABLE office.users (
-                                  id SERIAL PRIMARY KEY,
-                                  username VARCHAR(255) NOT NULL UNIQUE,
-                                  password VARCHAR(255) NOT NULL,
-                                  role VARCHAR(50) NOT NULL DEFAULT 'CUSTOMER' CHECK (role IN ('ADMIN', 'CUSTOMER')));
+                              id SERIAL PRIMARY KEY,
+
+                              username VARCHAR(100) NOT NULL UNIQUE,
+                              password VARCHAR(100) NOT NULL,
+
+                              role VARCHAR(20) NOT NULL
+                                  CHECK (role IN ('ADMIN', 'CUSTOMER')),
+
+                              created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                              updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_user_role ON office.users (role);
+CREATE INDEX idx_user_created ON office.users (created_at);
 
 CREATE TABLE office.day_rates (
                                   id SERIAL PRIMARY KEY,
